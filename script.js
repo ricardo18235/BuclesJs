8e82c4b1e279f43ee2246c9c6e20c3ab7e591916
ricardo18235 <registros18235@gmail.com>
2023-10-09 21:59:57 -0500

    creacion de practica ejercicios bucleJs

---
+++ script.js
@@ -0,0 +1,17 @@
+// Ejercicio 1
+console.log("------Ejercicio 1------");
+
+let numero = prompt("Ingrese el número a multiplicar");
+
+
+
+for (let i = 1; i <= 10; i++) {
+    // console.log(`${numero} x ${i} = ${numero * i}`);
+    console.log(numero + "x" + i + "="+ numero * i );
+}
+
+// Ejercicio 2
+
+console.log("------Ejercicio 2------");
+
+
