bb75a740184f12d6e5ae64424cde4a52c92036d0
ricardo18235 <registros18235@gmail.com>
2023-10-10 01:36:12 -0500

    se realizo el ejercicio 2

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
 // Ejercicio 1
 console.log("------Ejercicio 1------");
 
-let numero = prompt("Ingrese el número a multiplicar");
+let numeroEjercicioUno = prompt("Ingrese el número a multiplicar");
 
 
 
-for (let i = 1; i <= 10; i++) {
-    // console.log(`${numero} x ${i} = ${numero * i}`);
-    console.log(numero + "x" + i + "="+ numero * i );
+for (let i = 0; i <= 10; i++) {
+    console.log(` ${numeroEjercicioUno} x ${i} = ${numeroEjercicioUno * i} `);
 }
 
 // Ejercicio 2
-
 console.log("------Ejercicio 2------");
+
+let numeroGuardado = [];
+
+while (true) {
+    let numeroEjercicioDos = parseInt(prompt("Ingresa un número :"));
+
+    if (numeroEjercicioDos === 0) {
+        break; 
+    }
+
+    numeroGuardado.push(numero);
+}
+alert ("Ingreso el numero 0 se cerrara la aplicacion")
+console.log("Números ingresados:", numeroGuardado);
+
+// Ejercicio 3
+
+console.log("------Ejercicio 3------");
+
 
 
